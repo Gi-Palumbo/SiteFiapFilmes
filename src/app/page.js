bcb1f1df62f270404786ed4cc6d82e696165bdb0
Gi-Palumbo <125066782+Gi-Palumbo@users.noreply.github.com>
2023-08-11 10:07:43 -0300

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return ( //JSX
+    <nav className="bg-zinc-500 p-2 flex gap-3 items-end" >
+      <h1 className="text-4xl text-zinc-100 font-bold uppercase">
+        FIAP Filmes</h1>
+        <ul>
+          <li>
+            <a href='#'>Favoritos</a>
+          </li>
+        </ul>
+    </nav>
+  )
+}
